58a6461581d9b281e4563ea7f658a87ccaa19b9b
djkolesa@gmail.com <djkolesa@gmail.com>
2020-03-29 23:22:38 -0400

    David Kolesar is adding unit function (check this design out yo )

--- durationExercise.java
+++ durationExercise.java
@@ -1,10 +1,24 @@
 public class TimeFormatter {
     
     public static String formatDuration(int seconds) {
+       //edge case 
        if(seconds == 0)
        {
          return "now";
        }
+       
+       retVal = evaluateArgumentUnit();
+       
+       
        return "test";
     }
+    
+    
+    public static String evaluateArgumentUnit()
+    {
+    
+    }
+    
+    
+    
 }
