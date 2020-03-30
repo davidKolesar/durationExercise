fe6f24182077efa2fd07aca9b933c1aab9b5fc3a
djkolesa@gmail.com <djkolesa@gmail.com>
2020-03-29 23:41:04 -0400

    check it

--- durationExercise.java
+++ durationExercise.java
@@ -1,9 +1,11 @@
 public class TimeFormatter {
-private static final int HOUR = 3600;
-private static final int DAY = 86400;
-private static final int WEEK = 604800;
-private static final int MONTH = 2628288;
 
+  private static final int MINUTE = 60;
+  private static final int HOUR = 3600;
+  private static final int DAY = 86400;
+  private static final int WEEK = 604800;
+  private static final int MONTH = 2628288;
+  
     public static String formatDuration(int seconds) {
        //edge case 
        if(seconds == 0)
@@ -12,7 +14,7 @@ private static final int MONTH = 2628288;
        }
        
        //this should probably be an enum
-       int argumentUnit = evaluateArgumentUnit(seconds);
+       String argumentUnit = evaluateArgumentUnit(seconds);
        
        //switch statement
        
@@ -20,9 +22,34 @@ private static final int MONTH = 2628288;
     }
     
     
-    public static String evaluateArgumentUnit(seconds)
+    public static String evaluateArgumentUnit(int seconds)
     {
-    
+      
+
+      switch (seconds) {
+        case 1:
+          System.out.println("Monday");
+          break;
+        case 2:
+          System.out.println("Tuesday");
+          break;
+        case 3:
+          System.out.println("Wednesday");
+          break;
+        case 4:
+          System.out.println("Thursday");
+          break;
+        case 5:
+          System.out.println("Friday");
+          break;
+        case 6:
+          System.out.println("Saturday");
+          break;
+        case 7:
+          System.out.println("Sunday");
+          break;
+      }
+      
     
     }
     
