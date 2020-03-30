3649f8fd3e200a7fd3f32adf5c29a58e2f24d842
workComputer <djkolesa@gmail.com>
2020-03-29 23:17:33 -0400

    David Kolesar is adding basic work

---
+++ durationExercise.java
@@ -0,0 +1,10 @@
+public class TimeFormatter {
+    
+    public static String formatDuration(int seconds) {
+       if(seconds == 0)
+       {
+         return "now";
+       }
+       return "test";
+    }
+}
