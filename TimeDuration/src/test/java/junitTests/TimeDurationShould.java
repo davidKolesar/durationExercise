5273599c91f0cbe975bc465a121d2137fb8f1dba
unknown <djkolesa@gmail.com>
2020-07-04 11:40:42 -0600

    David Kolesar is adding more tests

--- TimeDuration/src/test/java/junitTests/TimeDurationShould.java
+++ TimeDuration/src/test/java/junitTests/TimeDurationShould.java
@@ -34,18 +34,38 @@ public class TimeDurationShould {
 	}
 
 	@Test
+	public void handleMinutesPlusSeconds() {
+		assertEquals("2 minutes and 2 seconds", Main.formatDuration(122));
+	}
+
+	@Test
+	public void handleMinutesPlusSecond() {
+		assertEquals("2 minutes and 1 second", Main.formatDuration(121));
+	}
+	
+	@Test
 	public void handleTwoMinutes() {
 		assertEquals("2 minutes", Main.formatDuration(120));
 	}
 
 	@Test
-	public void handleOneJHour() {
+	public void handleOneHour() {
 		assertEquals("1 hour", Main.formatDuration(3600));
+	}
+
+	@Test
+	public void handleTwoHours() {
+		assertEquals("2 hours", Main.formatDuration(7200));
 	}
 
 	@Test
 	public void handleOneHourPlusMinutesPlusSeconds() {
 		assertEquals("1 hour, 1 minute and 2 seconds", Main.formatDuration(3662));
+	}
+
+	@Test
+	public void handleOneHourPlusMinutesPlusOneSecond() {
+		assertEquals("1 hour, 1 minute and 1 second", Main.formatDuration(3661));
 	}
 
 	@Test
@@ -57,7 +77,6 @@ public class TimeDurationShould {
 	public void handleWeeksPlusMinutePlusSeconds() {
 		assertEquals("1 week, 1 minute and 5 seconds", Main.formatDuration(604865));
 	}
-
 
 	@Test
 	public void handleWeeksPlusMinutesPlusSeconds() {
